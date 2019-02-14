75f88f0419258c103ff79fa969d176dded1d5cb3
Edson Feimberg <feimberg@protonmail.com>
2019-02-14 21:00:20 -0200

    implements image uploading and resizing

--- classes/images.php
+++ classes/images.php
@@ -1,5 +1,8 @@
 <?php
 
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
 class images {
 
     public function uploadImage($image){
@@ -8,7 +11,32 @@ class images {
 
         if(in_array($tipo, array('image/jpeg', 'image/png'))){
 
-            move_uploaded_file($image['tmp_name'], 'assets/images/123.jpg');
+            $tempname= rand(0, 9999).'jpg';
+            move_uploaded_file($image['tmp_name'], 'assets/images/'.$tempname);
+
+            list($width_orig, $height_orig) = getimagesize('assets/images/'.$tempname);
+            $ratio= $width_orig/$height_orig;
+
+            $width=640;
+            $height=908;
+
+            if($width/$height > $ratio){
+                $width= $height*$ratio;
+            }else{
+                $height= $width/$ratio;
+            }
+
+
+            $img= imagecreatetruecolor($width, $height);
+            if($tipo== 'image/jpeg'){
+                $origi= imagecreatefromjpeg('assets/images/'.$tempname);
+            }elseif($tipo=='image/png'){
+                $origi= imagecreatefrompng('assets/images/'.$tempname);
+            }
+
+            imagecopyresampled($img, $origi, 0, 0, 0, 0, $width, $height, $width_orig, $height_orig);
+            imagejpeg($img, 'assets/images/'.$tempname, 80);
+            
         }
 
         
